b463b86b46e7c5cec6374c3469cbe6ce5ffba332
Mbalentlediko <dikombalentle9@gmail.com>
2024-05-23 10:03:05 +0200

    Task

---
+++ script/code.js
@@ -0,0 +1,16 @@
+function greet(greeting) {
+    console.log(greeting);
+}
+greet('Hello')
+
+function multiplication(numb1,numb2) {
+    console.log(numb1*numb2);
+}
+multiplication(58,5)
+
+function tableOfFive(multiplicant) {
+  let Answer = multiplicant*5
+  console.log(Answer);
+  return Answer
+}
+tableOfFive(8)
